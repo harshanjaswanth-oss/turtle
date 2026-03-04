bb4ea6f3c98a96b2f533a0805205fbdf4a8078cb
harshanjaswanth-oss <Jaswanth902>
2026-03-04 15:36:36 +0530

    star

---
+++ star.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.Screen().bgcolor("teal")
+turtle.Screen().setup(400,400)
+
+star=turtle.Turtle()
+for i in range(5):
+    star.forward(100)
+    star.left(144)
+turtle.done()
+            
+
